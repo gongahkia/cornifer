cb4243616267283486f90792d1214da65767a1ce
gongahkia <angryapplegravy@gmail.com>
2024-12-28 10:14:37 +0800

    feat(app): first pass

--- src/vision/main.py
+++ src/vision/main.py
@@ -1,5 +1,6 @@
 # ----- REQUIRED IMPORTS -----
 
+import json
 import sight as s
 import helper as he
 
@@ -9,4 +10,9 @@ if __name__ == "__main__":
     ROOT_PATH = "./boards_images/"
     TEST_PATH_ARRAY = he.list_folder_files(ROOT_PATH)
     print(TEST_PATH_ARRAY)
-    s.identify_contours(TEST_PATH_ARRAY, ROOT_PATH)
+    result_tuple = s.identify_contours(TEST_PATH_ARRAY, ROOT_PATH)
+    if result_tuple[0]:
+        print(result_tuple[1])
+        FINAL = "./nice.json"
+        with open(FINAL, "w") as f:
+            json.dump(result_tuple[1], f, indent=4)
--- src/vision/sight.py
+++ src/vision/sight.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+import os
 
 
 def render(target_filepath, mask):
@@ -24,6 +25,7 @@ def identify_contours(target_filepath_array, root_path, output_path="./identifie
     """
     identify contours in a specified image and write them to an output path
     """
+    contour_data = {}
     try:
         for filepath in target_filepath_array:
             image = cv2.imread(filepath)
@@ -36,13 +38,50 @@ def identify_contours(target_filepath_array, root_path, output_path="./identifie
                 thresholded_image, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE
             )
             mask = np.zeros_like(image)
+            contour_coordinates = []
             for i in range(len(contours)):
                 if hierarchy[0][i][3] != -1:
                     cv2.drawContours(mask, contours, i, (0, 255, 0), 2)
+                    contour_coords = contours[i].reshape(-1, 2)
+                    contour_coordinates.append(contour_coords.tolist())
             modified_file_path = f"{output_path}{filepath[len(root_path):]}"
             cv2.imwrite(modified_file_path, mask)
             # render(modified_file_path, mask)
-        return True
-    except:
-        print("Error: Unable to run identify_contours")
-        return False
+            contour_data[os.path.basename(filepath)] = contour_coordinates
+        print("Success: Contours identified successfully")
+        return (True, contour_data)
+    except Exception as e:
+        print(f"Error: Unable to run identify_contours: {e}")
+        return (False, None)
+
+
+def hold_selection_wrapper():
+    """
+    wrapper function for reading hold selection input
+    """
+
+    holds_array = []
+
+    def mouse_callback(event, x, y, holds_array, flags, param):
+        """
+        helper function that handles user hold selection
+        """
+        if event == cv2.EVENT_LBUTTONDOWN:
+            holds_array.append((x, y))
+            print(f"Selected hold at: ({x}, {y})")
+        return holds_array
+
+    def render_holds_image(selected_holds, target_filepath):
+        """
+        helper function that renders an image with opencv and allows users to select holds
+        """
+        image = cv2.imread(target_filepath)
+        cv2.namedWindow("Image")
+        cv2.setMouseCallback("Image", mouse_callback)
+        while True:
+            cv2.imshow("Image", image)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+        cv2.destroyAllWindows()
+        return selected_holds
